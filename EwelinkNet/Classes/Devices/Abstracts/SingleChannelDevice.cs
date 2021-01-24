76885958b0aef99afba855252886626d092b91a7
Luca Di Stefano <luca@distefano.bz.it>
2021-01-25 00:13:15 +0100

    change inheritance of powerswitchdevice

--- EwelinkNet/Classes/Devices/Abstracts/SingleChannelDevice.cs
+++ EwelinkNet/Classes/Devices/Abstracts/SingleChannelDevice.cs
@@ -1,9 +1,7 @@
 ﻿using EwelinkNet.Helpers.Extensions;
 using System;
-using System.Collections.Generic;
 using System.Dynamic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace EwelinkNet.Classes
